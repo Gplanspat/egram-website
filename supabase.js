3c7cde14a529018fb9ffc226ce3fb1e742d0452c
George <support@egramapp.com>
2026-08-13 10:38:45 +0100

    Fix Supabase client

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://gqkrchcgvggskgiojtvf.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_iA8BBvcwRsxfUWDYIsPlhw_ULkH8ipS";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
