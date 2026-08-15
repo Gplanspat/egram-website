f3f0c57aa424ac57c5c43f5a260e9f14785d0d54
George <support@egramapp.com>
2026-08-15 20:30:35 +0100

    Add 11-slide hero carousel and fix hero images

--- script.js
+++ script.js
@@ -107,13 +107,18 @@ const heroSlide = document.getElementById("heroSlide");
 
 if (heroSlide) {
   const slides = [
-    "images/hero/slide6.png",
-    "images/hero/slide7.png",
-    "images/hero/slide8.png",
-    "images/hero/slide9.png",
-    "images/hero/slide10.png",
-    "images/hero/slide11.png"
-  ];
+  "images/hero/slide1.png",
+  "images/hero/slide2.png",
+  "images/hero/slide3.png",
+  "images/hero/slide4.png",
+  "images/hero/slide5.png",
+  "images/hero/slide6.png",
+  "images/hero/slide7.png",
+  "images/hero/slide8.png",
+  "images/hero/slide9.png",
+  "images/hero/slide10.png",
+  "images/hero/slide11.png"
+];
 
   let current = 0;
   setInterval(() => {
